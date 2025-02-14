87d4fb1a2e6d0d605421839933b0d2c7192a77ef
Felipe Lima <lipr2024@gmail.com>
2025-02-14 08:38:21 -0300

    Update monitorestartservice.sh

--- monitorestartservice.sh
+++ monitorestartservice.sh
@@ -5,8 +5,8 @@ HOST=$(cat /../proc/sys/kernel/hostname)
 #service name received as a parameter when executing the script, ex: "./script.sh ssh"
 SERVICE="$1"
 #Telegram API Communication bot
-BOTTOKEN="7751708607:AAE3K8zE0ELqOUlMmuHP0IOJt3qWX9bgnXY"
-CHAT_ID="-1002388946900"
+BOTTOKEN="your-bot-token"
+CHAT_ID="your-chat-id"
 
 #Checks if the service is active, if not, starts it and sends a notification via telegram.
 if systemctl is-active --quiet "${SERVICE}"; then
